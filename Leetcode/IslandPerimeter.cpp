df53a53acc59b07e73beac8e5f70685914b09f0d
amisha-w <amishabw@gmail.com>
2020-10-01 19:27:57 +0530

    Added Leetcode progs

---
+++ Leetcode/IslandPerimeter.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int ans=0;
+        for(int i=0; i<grid.size(); i++)
+        {
+            for(int j=0; j<grid[0].size(); j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    if(i==0)
+                    {
+                        ans++;
+                    }else{
+                        if(grid[i-1][j]!=1)
+                            ans++;
+                    }
+                    if(j==0)
+                    {
+                        ans++;
+                    }else{
+                        if(grid[i][j-1]!=1)
+                            ans++;
+                    }
+                    if(i==grid.size()-1)
+                    {
+                        ans++;
+                    }else{
+                        if(grid[i+1][j]!=1)
+                            ans++;
+                    }
+                    if(j==grid[0].size()-1)
+                    {
+                        ans++;
+                    }else{
+                        if(grid[i][j+1]!=1)
+                            ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
